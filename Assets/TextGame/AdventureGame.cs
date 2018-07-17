54b3ebe00783d96bfac269cf9dd2949125ba6bad
JAKES <istakezx@gmail.com>
2018-07-17 02:46:25 -0400

    State Machine Updates

---
+++ Assets/TextGame/AdventureGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour {
+
+    [SerializeField] Text textComponent;
+
+	void Start ()
+    {
+        textComponent.text = ("You wake up from a loud bang, you yourself in a empty white room with 2 doors. The doors are labeled as Fantasy Land and Real World");
+	}
+	
+
+	void Update ()
+    {
+		
+	}
+}
