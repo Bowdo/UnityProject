219bcd512ccf7439fc33de0f5ec57046749f88af
JAKES <istakezx@gmail.com>
2018-07-17 03:13:10 -0400

    Array Updates

--- Assets/TextGame/AdventureGame.cs
+++ Assets/TextGame/AdventureGame.cs
@@ -8,12 +8,18 @@ public class AdventureGame : MonoBehaviour {
     [SerializeField] Text textComponent;
     [SerializeField] State startingState;
 
+    int[] oddNumbers = { 1, 3, 5, 7, 9 };
+    string[] firstNames = { "Jake", "John", "Nick", "Fred" };
+
     State state;
 
 	void Start ()
     {
         state = startingState;
         textComponent.text = state.GetStateStory();
+
+        Debug.Log(oddNumbers[4]);
+        Debug.Log(firstNames[2]);
 	}
 	
 
